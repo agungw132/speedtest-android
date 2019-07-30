5a9fac2115b7908d2d7092152809771fa9583633
Agung Wahyudi <agung.wahyudi@gmail.com>
2019-07-30 18:10:34 +0200

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+id=`shuf -i 1-100000000 -n 1`
 location=`termux-location -r last` 
 echo $location
-mosquitto_pub -h broker.hivemq.com -t topic -m "$location"
+mosquitto_pub -h broker.hivemq.com -t tele/broker/$id/location -m "$location"
 wifi=`termux-wifi-connectioninfo`
 echo $wifi
-mosquitto_pub -h broker.hivemq.com -t topic -m "$wifi"
+mosquitto_pub -h broker.hivemq.com -t tele/broker/$id/wifi -m "$wifi"
 result=`speedtest-cli --json`
 echo $result
-mosquitto_pub -h broker.hivemq.com -t topic -m "$result"
+mosquitto_pub -h broker.hivemq.com -t tele/broker/$id/speedtest -m "$result"
